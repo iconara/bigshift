bad29bd885880a223ed09331bbdeafc31b2b36d2
Theo <theo@iconara.net>
2016-03-15 08:50:18 +0100

    Prepare to rename BigQueryDataset to BigQuery::Dataset

--- lib/rs2bq.rb
+++ lib/rs2bq.rb
@@ -20,7 +20,7 @@ module RS2BQ
   end
 end
 
-require 'rs2bq/big_query_dataset'
+require 'rs2bq/big_query/dataset'
 require 'rs2bq/big_query_schema_builder'
 require 'rs2bq/big_query_table_creator'
 require 'rs2bq/redshift_table_schema'
