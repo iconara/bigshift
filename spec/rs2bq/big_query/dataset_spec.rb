520d544bc0331d8e8868cc6a0c345ffeb0eeca19
Theo <theo@iconara.net>
2016-03-15 08:51:23 +0100

    Rename BigQueryDataset BigQuery::Dataset

--- spec/rs2bq/big_query/dataset_spec.rb
+++ spec/rs2bq/big_query/dataset_spec.rb
@@ -1,76 +1,78 @@
 module RS2BQ
-  describe BigQueryDataset do
-    let :dataset do
-      described_class.new(big_query_service, 'my_project', 'my_dataset')
-    end
-
-    let :big_query_service do
-      double(:big_query_service)
-    end
-
-    let :table do
-      double(:table)
-    end
-
-    before do
-      allow(big_query_service).to receive(:insert_table)
-      allow(big_query_service).to receive(:get_table).with('my_project', 'my_dataset', 'my_table').and_return(table)
-    end
-
-    describe '#table' do
-      it 'returns the specified table' do
-        expect(dataset.table('my_table')).to eq(table)
+  module BigQuery
+    describe Dataset do
+      let :dataset do
+        described_class.new(big_query_service, 'my_project', 'my_dataset')
       end
 
-      context 'when the table does not exist' do
-        before do
-          allow(big_query_service).to receive(:get_table).and_raise(Google::Apis::ClientError.new('Bork', status_code: 404))
-        end
-
-        it 'returns nil' do
-          expect(dataset.table('my_table')).to be_nil
-        end
+      let :big_query_service do
+        double(:big_query_service)
       end
 
-      context 'when an non-not-found error is raised' do
-        before do
-          allow(big_query_service).to receive(:get_table).and_raise(Google::Apis::ClientError.new('Bork', status_code: 500))
-        end
-
-        it 're-raises the error' do
-          expect { dataset.table('my_table') }.to raise_error(Google::Apis::ClientError)
-        end
-      end
-    end
-
-    describe '#create_table' do
-      it 'creates the table' do
-        table_reference = nil
-        allow(big_query_service).to receive(:insert_table) do |_, _, table|
-          table_reference = table.table_reference
-        end
-        dataset.create_table('my_table')
-        expect(table_reference.project_id).to eq('my_project')
-        expect(table_reference.dataset_id).to eq('my_dataset')
-        expect(table_reference.table_id).to eq('my_table')
+      let :table do
+        double(:table)
       end
 
-      it 'creates the table in the specified project and dataset' do
-        dataset.create_table('my_table')
-        expect(big_query_service).to have_received(:insert_table).with('my_project', 'my_dataset', anything)
+      before do
+        allow(big_query_service).to receive(:insert_table)
+        allow(big_query_service).to receive(:get_table).with('my_project', 'my_dataset', 'my_table').and_return(table)
       end
 
-      context 'when a schema is specified' do
-        it 'passes it along' do
-          schema = {'fields' => [{'name' => 'id', 'type' => 'INTEGER', 'mode' => 'REQUIRED'}, {'name' => 'fax_number', 'type' => 'STRING', 'mode' => 'NULLABLE'}]}
-          table_schema = nil
-          allow(big_query_service).to receive(:insert_table) do |_, _, table|
-            table_schema = table.schema
+      describe '#table' do
+        it 'returns the specified table' do
+          expect(dataset.table('my_table')).to eq(table)
+        end
+
+        context 'when the table does not exist' do
+          before do
+            allow(big_query_service).to receive(:get_table).and_raise(Google::Apis::ClientError.new('Bork', status_code: 404))
           end
-          dataset.create_table('my_table', schema: schema)
-          expect(table_schema.fields.map(&:name)).to eq(%w[id fax_number])
-          expect(table_schema.fields.map(&:type)).to eq(%w[INTEGER STRING])
-          expect(table_schema.fields.map(&:mode)).to eq(%w[REQUIRED NULLABLE])
+
+          it 'returns nil' do
+            expect(dataset.table('my_table')).to be_nil
+          end
+        end
+
+        context 'when an non-not-found error is raised' do
+          before do
+            allow(big_query_service).to receive(:get_table).and_raise(Google::Apis::ClientError.new('Bork', status_code: 500))
+          end
+
+          it 're-raises the error' do
+            expect { dataset.table('my_table') }.to raise_error(Google::Apis::ClientError)
+          end
+        end
+      end
+
+      describe '#create_table' do
+        it 'creates the table' do
+          table_reference = nil
+          allow(big_query_service).to receive(:insert_table) do |_, _, table|
+            table_reference = table.table_reference
+          end
+          dataset.create_table('my_table')
+          expect(table_reference.project_id).to eq('my_project')
+          expect(table_reference.dataset_id).to eq('my_dataset')
+          expect(table_reference.table_id).to eq('my_table')
+        end
+
+        it 'creates the table in the specified project and dataset' do
+          dataset.create_table('my_table')
+          expect(big_query_service).to have_received(:insert_table).with('my_project', 'my_dataset', anything)
+        end
+
+        context 'when a schema is specified' do
+          it 'passes it along' do
+            schema = {'fields' => [{'name' => 'id', 'type' => 'INTEGER', 'mode' => 'REQUIRED'}, {'name' => 'fax_number', 'type' => 'STRING', 'mode' => 'NULLABLE'}]}
+            table_schema = nil
+            allow(big_query_service).to receive(:insert_table) do |_, _, table|
+              table_schema = table.schema
+            end
+            dataset.create_table('my_table', schema: schema)
+            expect(table_schema.fields.map(&:name)).to eq(%w[id fax_number])
+            expect(table_schema.fields.map(&:type)).to eq(%w[INTEGER STRING])
+            expect(table_schema.fields.map(&:mode)).to eq(%w[REQUIRED NULLABLE])
+          end
         end
       end
     end
