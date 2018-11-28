0c19085622d616b25ee0661094f53ecbb397da3a
Mike Fowler <mike@bashton.com>
2018-11-28 13:18:26 +0000

    Simplify error reporting
    When `error.location` doesn't contain a string that matches the expected
    output a `nil` is encountered causing a new error to propagate losing
    the one we're interested in. Instead simply report the value of
    `error.location` if it is present.

--- lib/bigshift/big_query/table.rb
+++ lib/bigshift/big_query/table.rb
@@ -36,11 +36,7 @@ module BigShift
             else
               job.status.errors.each do |error|
                 message = %<Load error: "#{error.message}">
-                if error.location
-                  file, line, field = error.location.split('/').map { |s| s.split(':').last.strip }
-                  message << " at file #{file}, line #{line}"
-                  message << ", field #{field}" if field
-                end
+                message << " in #{error.location}" if error.location
                 @logger.debug(message)
               end
               raise job.status.error_result.message
